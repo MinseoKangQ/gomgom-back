0e367461fdacc6b70f5f6973fb09ebfffa749aee
MinseoKangQ <kms02171@naver.com>
2023-07-01 17:48:13 +0900

    곰곰이 페이지 back 완성

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from gomgom.views import ai
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('gomgom/ai/', ai),
 ]
