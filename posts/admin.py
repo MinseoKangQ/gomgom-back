18f26a6998f8a7c58fcd7dda6d858c4b2a95693b
MinseoKangQ <kms02171@naver.com>
2023-07-05 23:25:21 +0900

    Comment 모델을 admin에 등록

--- posts/admin.py
+++ posts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 
-from .models import Post,Selection
+from .models import Post, Selection, Comment
 
 @admin.register(Post)
 class UserModelAdmin(admin.ModelAdmin):
@@ -10,3 +10,11 @@ class UserModelAdmin(admin.ModelAdmin):
 @admin.register(Selection)
 class UserModelAdmin(admin.ModelAdmin):
     pass
+
+@admin.register(Comment)
+class UserModelAdmin(admin.ModelAdmin):
+    pass
+
+# admin.site.register(Post)
+# admin.site.register(Selection)
+# admin.site.register(Comment)
